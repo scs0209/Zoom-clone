c112a8a27a8aff4e94e3ae92038d35a9f9d45384
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-09-14 23:35:41 +0900

    JSON.stringify 사용(객체를 string으로 보내는 역할)

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,25 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 // backend로 메세지를 보내고 받을 수 있다.
 //app.js의 socket은 서버로의 연결을 뜻함
 const socket = new WebSocket(`ws://${window.location.host}`);//backend 연결
 
+
+function makeMessage(type, payload){
+  const msg = {type, payload};
+  // JSON.stringify는 object를 string으로 변경해서 보냄
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 //오프라인이 됐을 때 사용하는 listener
@@ -23,8 +32,15 @@ function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
   //backend로 보내고 메세지를 보냄
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
